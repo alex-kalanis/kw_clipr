747bcbcc5f8f2bab374eb0174bb74114138d26c4
Petr Plsek <me@kalanys.com>
2024-04-02 21:05:18 +0200

    kw_clipr - update dependency check 3

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -14,6 +14,6 @@ return $config
     // ignore errors on specific packages and paths
     ->ignoreErrorsOnPackageAndPath('psr/container', __DIR__ . '/php-src/Loaders/DiLoader.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
     ->addPathToScan(__DIR__ . '/run', false)
-    ->ignoreUnknownClasses(['\kalanis\kw_autoload\DependencyInjection'])
+    ->ignoreUnknownClasses(['kalanis\kw_autoload\DependencyInjection'])
 //    ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_autoload', __DIR__ . '/php-src/Loaders/KwDiLoader.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
 ;
